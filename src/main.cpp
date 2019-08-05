0ce804a8e5810bafec417a99fc11d8f30074f850
MaySantucci <may.santucci@gmail.com>
2019-08-05 11:35:44 +0200

    Registered pointsModel as type in main.

--- src/main.cpp
+++ src/main.cpp
@@ -35,8 +35,7 @@ int main(int argc, char *argv[])
 
     engine.addImportPath("qrc:/style");
 
-    PointsModel pointsModel;
-    engine.rootContext()->setContextProperty("pointsModel", &pointsModel);
+    qmlRegisterType<PointsModel>("PointsModel", 1, 0, "PointsModel");
 
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
