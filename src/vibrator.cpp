ff73345ad062d1c0158613c7c311630a7bcc3b8f
Federico Frenguelli <synasius@gmail.com>
2019-08-06 14:50:48 +0200

    Added vibration function for iOS device.

--- src/vibrator.cpp
+++ src/vibrator.cpp
@@ -1,6 +1,12 @@
 #include "vibrator.h"
 
+#ifdef Q_OS_ANDROID
 #include <QtAndroidExtras>
+#endif
+
+#ifdef Q_OS_IOS
+#include "ios/service/vibratorservice.h"
+#endif
 
 Vibrator::Vibrator(QObject *parent) : QObject(parent)
 {
@@ -8,10 +14,15 @@ Vibrator::Vibrator(QObject *parent) : QObject(parent)
 }
 
 void Vibrator::vibrate() {
+#ifdef Q_OS_ANDROID
     // Retrive activity
     QAndroidJniObject activity = QtAndroid::androidActivity();
     if(activity.isValid()) {
         activity.callMethod<void>("vibrate", "()V");
     }
+#endif
 
+#ifdef Q_OS_IOS
+    iOSVibrate();
+#endif
 }
