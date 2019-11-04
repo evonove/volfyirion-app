07a74af6c827bcfbe5c02a7fac1d178fbe89b2e0
Federico Frenguelli <synasius@gmail.com>
2019-11-04 12:48:51 +0100

    Added toast message.

--- src/ios/service/photosaverservice.mm
+++ src/ios/service/photosaverservice.mm
@@ -11,46 +11,19 @@ void PhotoSaverService::showToast(const QString &message) {
     qCritical() << "PHOTO SAVER: showToast() " << message;
     NSString* messageToast = message.toNSString();
     qCritical() << messageToast;
-//    [[NSOperationQueue mainQueue] addOperationWithBlock: ^ {
-//        UIWindow *keyWindow = [[UIApplication sharedApplication] keyWindow];
-//        UILabel *toastView = [[UILabel alloc] init];
 
-//        toastView.text = messageToast;
-//        toastView.frame = CGRectMake(0.0, 0.0, keyWindow.frame.size.width/2.0, 200.0);
-//        toastView.textAlignment = NSTextAlignmentCenter;
-//        toastView.layer.cornerRadius = 10;
-//        toastView.layer.masksToBounds = YES;
-//        toastView.backgroundColor = [UIColor grayColor];
-//        toastView.textColor = [UIColor whiteColor];
-//        toastView.center = keyWindow.center;
+    [[NSOperationQueue mainQueue] addOperationWithBlock: ^{
 
-//        [keyWindow addSubview:toastView];
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@""
+                message: messageToast
+                preferredStyle: UIAlertControllerStyleAlert];
 
-//        [UIView animateWithDuration: 3.0f
-//                          delay: 0.0
-//                        options: UIViewAnimationOptionCurveEaseOut
-//                     animations: ^{
-//                         toastView.alpha = 0.0;
-//                     }
-//                     completion: ^(BOOL finished) {
-//                         [toastView removeFromSuperview];
-//                     }
-//         ];
-//    }];
+        UIAlertAction *defaultAction = [UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {}];
+        [alert addAction:defaultAction];
 
-//    dispatch_async(dispatch_get_main_queue(), ^{
-//        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Artworks"
-//                                                      message: messageToast
-//                                                      preferredStyle: UIAlertControllerStyleActionSheet ];
-
-//        UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
-//                                //button click event
-//                            }];
-//        UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
-//        [alert addAction:cancel];
-//        [alert addAction:ok];
-//        [alert presentViewController:alert animated:YES completion:nil];
-//    });
+        // We use [UIApplication sharedApplication].keyWindow.rootViewController instead of 'self' to take the pointer to the current instance.
+        [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
+    }];
 }
 
 bool PhotoSaverService::checkWritingPermission() {
@@ -60,14 +33,14 @@ bool PhotoSaverService::checkWritingPermission() {
 
             QString message = "Permission not granted to save artwork in Photos";
             result = false;
-            PhotoSaverService::showToast(message);
+            showToast(message);
 
             printf("CHECK IOS: Permission not granted.");
 
         } else {
             QString message = "Permission granted to save artwork in Photos";
             result = true;
-            PhotoSaverService:showToast(message);
+            showToast(message);
             printf("CHECK IOS: Permission granted.");
         }
     }];
@@ -87,11 +60,11 @@ void PhotoSaverService::saveImageInPhotos(QImage artwork) {
         [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
             if(status != PHAuthorizationStatusAuthorized) {
                 QString message = "Permission not granted to save artwork in Photos";
-                PhotoSaverService::showToast(message);
+                showToast(message);
                 printf("CHECK IOS: Permission not granted.");
             } else {
                 QString message = "Permission granted to save artwork in Photos";
-                PhotoSaverService:showToast(message);
+                showToast(message);
                 printf("CHECK IOS: Permission granted.");
 
                 qCritical() << "artwork is valid: " << artwork.size();
@@ -118,11 +91,16 @@ void PhotoSaverService::performChangesInPhotosLibrary(QImage artwork){
             PHAssetChangeRequest *createAssetRequest = [PHAssetChangeRequest creationRequestForAssetFromImage:image];
             PHObjectPlaceholder *assetPlaceHolder = [createAssetRequest placeholderForCreatedAsset];
         } completionHandler:^ (BOOL success, NSError *error) {
-            qCritical() << "Finished adding asset success:" <<  success;
-            qCritical() << "Finished adding asset error:" <<  error;
+            if(success) {
+                qCritical() << "Finished adding asset success:" <<  success;
+                QString message = "Artwork saved in Photos Library.";
+                showToast(message);
+            }
         }];
     } else {
-        qCritical() << "Error occurs during conversion of Image.";
+        auto errorMessage = "Error occurs during conversion of Image.";
+        qCritical() << errorMessage;
+        showToast(errorMessage);
     }
 
 }
